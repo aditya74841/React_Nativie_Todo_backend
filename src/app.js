1b2328994a6cab61b03aec94511a3b03abc84fe7
Aditya Ranjan <aditya74810@gmail.com>
2025-02-10 21:13:27 +0530

    added home router

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 import express from "express";
 import { createServer } from "http";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 
 const httpServer = createServer(app);
 
 app.use(
-    cors({
-      origin: [     
-       
-        "*"
-      ],
-     
-    })
-  );
+  cors({
+    origin: ["*"],
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -22,5 +18,7 @@ app.use(express.static("public")); // configure static file to save images local
 import todoRouter from "../src/routes/todo.routes.js";
 
 app.use("/api/v1/todo", todoRouter);
-
+app.get("/", (req, res) => {
+  console.log("Server is running at home")
+});
 export { httpServer };
